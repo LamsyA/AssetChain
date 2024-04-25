aed06e59970c14d649c7df9a1a979adc959948df
Lamsy <olamide.adetula.1744@gmail.com>
2024-04-25 13:52:56 +0100

    ⛏  : hero page .....

--- pages/components/Navbar/Navbar.tsx
+++ pages/components/Navbar/Navbar.tsx
@@ -3,7 +3,7 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 function Navbar() {
   return (
     <div>
-      <div className="sticky top-0 z-50  bg-gradient-to-l from-emerald-200 to-emerald-400 shadow-lg shadow-slate-300 text-emerald-700  dark:text-white dark:bg-slate-600">
+      <div className="sticky top-0 z-50  bg-gradient-to-r from-orange-400  to-emerald-400 shadow-lg shadow-slate-300 text-emerald-700  dark:text-white dark:bg-slate-600">
         <div className="  py-4 broder-b-[1px]">
           <div className="flex justify-around">
             <div className="text-3xl text-white font-bold">AssetChain</div>
