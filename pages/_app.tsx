1365c891eb40957ecd41ba2dab58ddf810fdbabe
Lamsy <olamide.adetula.1744@gmail.com>
2024-04-25 09:21:35 +0100

    ⛏ 🛠️ : loading.....

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,11 @@
+'use client';
+
 import '../styles/globals.css';
 import '@rainbow-me/rainbowkit/styles.css';
 import type { AppProps } from 'next/app';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { WagmiProvider } from 'wagmi';
+import { WagmiProvider, http } from 'wagmi';
 import {
   arbitrum,
   base,
@@ -12,21 +14,36 @@ import {
   polygon,
   sepolia,
 } from 'wagmi/chains';
-import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { getDefaultConfig, getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import {argentWallet, ledgerWallet, trustWallet} from "@rainbow-me/rainbowkit/wallets";
+
+const { wallets } = getDefaultWallets();
 
 const config = getDefaultConfig({
-  appName: 'RainbowKit App',
-  projectId: 'YOUR_PROJECT_ID',
-  chains: [
-    mainnet,
-    polygon,
-    optimism,
-    arbitrum,
-    base,
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [sepolia] : []),
-  ],
-  ssr: true,
-});
+  appName: 'Peeps: The decentralized platform',
+    projectId: '7f49c7e89e54528522eef8334c58506e',
+    transports: {
+        [mainnet.id]: http(""), // http('https://eth-mainnet.g.alchemy.com/v2/...')
+        [sepolia.id]: http("https://eth-sepolia.g.alchemy.com/v2/jRWeU9pFpeATDtbmRTHOuSuSp5OVVAO0"), // http('https://eth-sepolia.g.alchemy.com/v2/...')
+    },
+    wallets: [
+        ...wallets,
+        {
+            groupName: 'Other',
+            wallets: [argentWallet, trustWallet, ledgerWallet],
+        }
+    ],
+    chains: [
+        // mainnet,
+        sepolia,
+        // polygon,
+        // optimism,
+        // arbitrum,
+        base,
+        ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [sepolia] : []),
+    ],
+    ssr: true,
+})
 
 const client = new QueryClient();
 
