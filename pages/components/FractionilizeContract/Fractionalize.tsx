1e80072817141f93ec9e6040de25bcb245266828
Lamsy <olamide.adetula.1744@gmail.com>
2024-04-27 15:59:09 +0100

    integration

--- pages/components/FractionilizeContract/Fractionalize.tsx
+++ pages/components/FractionilizeContract/Fractionalize.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState, FormEvent } from "react";
 
 interface FractionalizeData {
   tokenId: string;
@@ -45,114 +45,104 @@ const FractionalizeForm: React.FC = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col w-full max-w-3xl mx-auto p-4 bg-gray-200 rounded-lg shadow-md">
-    <h2 className=" text-center mb-4 text-xl font-bold text-gray-800">Fractionalize Asset</h2>
-
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        <label htmlFor="tokenId" className="text-gray-700 font-medium mb-2">
-          Token ID:
-        </label>
-        <input
-          type="text"
-          id="tokenId"
-          name="tokenId"
-          value={formData.tokenId}
-          onChange={handleChange}
-          className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-        />
-
-        <label htmlFor="assetManager" className="text-gray-700 font-medium mb-2">
-          Asset Manager Address:
-        </label>
-        <input
-          type="text"
-          id="assetManager"
-          name="assetManager"
-          value={formData.assetManager}
-          onChange={handleChange}
-          className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-        />
-
-        <label htmlFor="assetName" className="text-gray-700 font-medium mb-2">
-          Asset Name:
-        </label>
-        <input
-          type="text"
-          id="assetName"
-          name="assetName"
-          value={formData.assetName}
-          onChange={handleChange}
-          className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-        />
-
-        <label htmlFor="assetSymbol" className="text-gray-700 font-medium mb-2">
-          Asset Symbol:
-        </label>
-        <input
-          type="text"
-          id="assetSymbol"
-          name="assetSymbol"
-          value={formData.assetSymbol}
-          onChange={handleChange}
-          className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-        />
-
-        <label htmlFor="assetPrice" className="text-gray-700 font-medium mb-2">
-          Asset Price:
-        </label>
-        <input
-          type="text"
-          id="assetPrice"
-          name="assetPrice"
-          value={formData.assetPrice}
-          onChange={handleChange}
-          className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-        />
-
-<label htmlFor="paymentToke" className="text-gray-700 font-medium mb-2">
-Payment Token Address:
-        </label>
-        <input
-          type="text"
-          id="paymentToken"
-          name="paymentToken"
-          value={formData.paymentToken}
-          onChange={handleChange}
-          className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-        />
-  
-  <label htmlFor="description" className="text-gray-700 font-medium mb-2">
-          Description:
-        </label>
-        <textarea
-        //   type="text"
-          id="description"
-          name="description"
-          value={formData.description}
-          onChange={handleChange}
-          className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-        /> 
-  
-  <label htmlFor="uri" className="text-gray-700 font-medium mb-2">
-          URI (Optional):
-        </label>
-        <input
-          type="text"
-          id="uri"
-          name="uri"
-          value={formData.uri}
-          onChange={handleChange}
-          className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-        />
-  
-       
-        <button type="submit" className="bg-teal-500 text-white px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-700">
-        Fractionalize
-      </button>
+    <form onSubmit={handleSubmit} className="container mx-auto px-4 sm:px-6 lg:px-8 py-12">
+      <div className="max-w-3xl mx-auto bg-gray-200 dark:bg-gray-800 rounded-lg shadow-md p-8">
+        <h2 className="text-center mb-4 text-xl font-bold text-gray-800 dark:text-white">Fractionalize Asset</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div className="flex flex-col">
+            <label htmlFor="tokenId" className="text-gray-700 font-medium mb-2">Token ID:</label>
+            <input
+              type="text"
+              id="tokenId"
+              name="tokenId"
+              value={formData.tokenId}
+              onChange={handleChange}
+              className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="assetManager" className="text-gray-700 font-medium mb-2">Asset Manager Address:</label>
+            <input
+              type="text"
+              id="assetManager"
+              name="assetManager"
+              value={formData.assetManager}
+              onChange={handleChange}
+              className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="assetName" className="text-gray-700 font-medium mb-2">Asset Name:</label>
+            <input
+              type="text"
+              id="assetName"
+              name="assetName"
+              value={formData.assetName}
+              onChange={handleChange}
+              className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="assetSymbol" className="text-gray-700 font-medium mb-2">Asset Symbol:</label>
+            <input
+              type="text"
+              id="assetSymbol"
+              name="assetSymbol"
+              value={formData.assetSymbol}
+              onChange={handleChange}
+              className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="assetPrice" className="text-gray-700 font-medium mb-2">Asset Price:</label>
+            <input
+              type="text"
+              id="assetPrice"
+              name="assetPrice"
+              value={formData.assetPrice}
+              onChange={handleChange}
+              className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="paymentToken" className="text-gray-700 font-medium mb-2">Payment Token Address:</label>
+            <input
+              type="text"
+              id="paymentToken"
+              name="paymentToken"
+              value={formData.paymentToken}
+              onChange={handleChange}
+              className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="description" className="text-gray-700 font-medium mb-2">Description:</label>
+            <textarea
+              id="description"
+              name="description"
+              value={formData.description}
+              onChange={handleChange}
+              className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="uri" className="text-gray-700 font-medium mb-2">URI (Optional):</label>
+            <input
+              type="text"
+              id="uri"
+              name="uri"
+              value={formData.uri}
+              onChange={handleChange}
+              className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            />
+          </div>
         </div>
-      </form>
-    );
-  };
-  
-  export default FractionalizeForm;
-  
+        <button type="submit" className="bg-teal-500 text-white px-4 py-2 rounded-md mt-4 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-700">
+          Fractionalize
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default FractionalizeForm;
--- pages/components/Homepage.tsx
+++ pages/components/Homepage.tsx
@@ -3,8 +3,10 @@ import Navbar from "./Navbar/Navbar";
 import Hero from "./Hero";
 import Register from "./Register/Register";
 import Admin from "./adminowner/Admin";
-import { WalletInstance } from "@rainbow-me/rainbowkit/dist/wallets/Wallet";
+import router, { useRouter } from 'next/router';
+import { getAccount } from '@wagmi/core'
 import FractionalizeForm from "./FractionilizeContract/Fractionalize";
+import Link from "next/link";
 function Homepage() {
   return (
     <div className="">
@@ -12,10 +14,18 @@ function Homepage() {
       <div className="">
         <Hero />
       </div>
-      {/* <Register/>  */}
-      {/* <Admin/> */}
       
-      <FractionalizeForm/>
+      {/* <Register/>  */}
+      <Admin/>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      type="button" onClick={() => router.push('/components/FractionilizeContract/Fractionalize/')}>
+        {/* <Link href="/components/FractionilizeContract/Fractionalize/"> */}
+          Fractionalize
+        {/* </Link> */}
+      </button>
+     
+      {/* <FractionalizeForm/> */}
+      
     </div>
   );
 }
