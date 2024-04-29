1496a40d72972c9e277a6cf46f7fc3b5312ca419
the-first-elder <the-first-elder@DESKTOP-IGU9BPA>
2024-04-29 14:28:42 +0100

    order book f.e

--- pages/components/FractionOrderBook/OrderBookGenerator.tsx
+++ pages/components/FractionOrderBook/OrderBookGenerator.tsx
@@ -7,7 +7,7 @@ import OrderBook from "./OrderBook";
 const OrderBookGenerator = () => {
  const { data: hash, writeContract } = useWriteContract();
  const [orders, setOrders] = useState([]); // State to store all orders
- const [tokenId, setTokenId] = useState(""); // State for the token ID
+ const [tokenState, setTokenState] = useState(true); // State for the token ID
 
  const { data: orderCount } = useReadContract({
     abi,
@@ -16,40 +16,39 @@ const OrderBookGenerator = () => {
  });
 
  const numericOrderCount = orderCount as number;
+
+if(orderCount && tokenState ){
+console.log("jii")
+  let allOrders: any = [];
+  for (let i = 0; i < numericOrderCount; i++) {
+    // Fetch each order asynchronously
+    const { data: orderStruct } = useReadContract({
+      abi,
+      address: `0x${FractionOrderContract}`,
+      functionName: "order",
+      args: [1],
+    });
+    allOrders.push(orderStruct); // Add fetched order data to the array
+  }
+  setOrders(allOrders);
+  setTokenState(false);
+}
+
  // Fetch order book data on component mount
  useEffect(() => {
-    const fetchAllOrders = async () => {
-      if (!orderCount) return; // Skip fetching if order count is not available
-
-      try {
-        const allOrders: any = [];
-        for (let i = 0; i < numericOrderCount; i++) {
-          // Fetch each order asynchronously
-          const { data: orderStruct } = await useReadContract({
-            abi,
-            address: `0x${FractionOrderContract}`,
-            functionName: "order",
-            args: [i],
-          });
-          allOrders.push(orderStruct); // Add fetched order data to the array
-        }
-        setOrders(allOrders); // Update state with all fetched orders
-      } catch (error) {
-        console.error("Error fetching orders:", error);
-      }
-    };
-
-    fetchAllOrders(); // Call the function to fetch orders
- }, [orderCount]); // Dependencies for effect
+  console.log("hi")
+    setTokenState(true);
+ }, []); // Dependencies for effect
 
  // Handle loading, error, and order data display logic here
 
  return (
-    <>
-    <OrderBook  orders={orders}/>
-      {/* Order book content using orders state goes here */}
-    </>
- );
+  <>
+    {orders.length === 0 ? null : <OrderBook orders={orders} />}
+    {/* Order book content using orders state goes here */}
+  </>
+);
+
 };
 
 export default OrderBookGenerator;
